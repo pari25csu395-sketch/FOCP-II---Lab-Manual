55b3da1c060b513b2a97e1ef844974c7986f16ac
Pari <pari25csu395@ncuindia.edu>
2026-02-01 01:14:07 +0530

    first commit

---
+++ 11.cpp
@@ -0,0 +1,13 @@
+ #include <iostream>
+using namespace std ;
+int main (){
+float marks, marks1, marks2, marks3;
+float avg= (mark1+marks2+marks3)/3.0;
+cout<< "enter 3 sub marks= \n";
+cin>> "marks1";
+cin>> "marks2";
+cin>> "marks3";
+cout<< "avg is=" <<avg << end;
+cout<< "average is=" <<avg << end ;
+return 0;
+}
